f3df0e37ced51672765a40e6607d2f1fa7e2e8c7
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-02-01 16:04:36 -0300

    ficha12 tiene m y b, inicio

---
+++ ficha12/con_m_y_b.py
@@ -0,0 +1,56 @@
+# Desarrollar un programa que permita ingresar por teclado, con palabras separadas por un espacio y terminado en punto. En base al texto ingresado, determinar:
+
+# Cuántas palabras tienen una m y una b a partir de la tercer letra.
+# Cuántas palabras comienzan con la letra p seguida de cualquier vocal.
+# Cuántas palabras comienzan y terminan con el mismo carácter.
+
+def tieneMB(letra):
+    if letra == 'm' or letra == 'b':
+        return True
+    return False
+
+def tieneVocal(letra):
+    vocales = 'aeiouáéíóúAEIOUÁÉÍÓÚ'
+    if letra in vocales:
+        return True
+    return False
+    
+    
+def principal():
+    posicion_letras = 0
+    tiene_m_b_desde_tercera_letra = 0
+    anterior = ''
+    palabra_comienza_p_sigue_vocal = 0
+    cant_palabras = 0
+    
+    texto = input('Ingrese el texto a analizar: ')
+    
+    for letra in texto:
+        
+        if letra == ' ' or letra == '.':
+            posicion_letras = 0
+            
+            
+        else:
+            posicion_letras += 1
+            
+            if posicion_letras >= 3 and tieneMB(letra):
+                tiene_m_b_desde_tercera_letra += 1
+                
+            if posicion_letras == 1 and letra == 'p':
+                anterior = letra
+                
+                
+            
+            if anterior and tieneVocal(letra):
+                 palabra_comienza_p_sigue_vocal += 1
+                
+            
+                
+    
+    
+    print(f'La cantidad de palabras que tienen "m" o "b" a partir de la tercera letra son: {tiene_m_b_desde_tercera_letra}')
+    print(f'La cantidad de palabras que comienzan con "p" y siguen con una vocal, son: {palabra_comienza_p_sigue_vocal}')
+    
+
+principal()
