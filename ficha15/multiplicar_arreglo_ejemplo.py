31232f7bdb26c727693e2ea5a7b6d29d0bf8cc97
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-02-23 11:18:04 -0300

    ficha15 ejemplos de impresion de arreglos

---
+++ ficha15/multiplicar_arreglo_ejemplo.py
@@ -0,0 +1,15 @@
+
+arreglo_1 = [7,8,9,4,5,6,1,2,10,1245,458]
+
+
+#ESTO IMPRIME LOS ELEMENTOS DEL ARREGLO
+for i in range(len(arreglo_1)):
+    print(arreglo_1[i])
+
+print('-' * 50)
+
+#ESTO IMPRIME LOS INDICES DEL ARREGLO
+for i in range(len(arreglo_1)):
+    print(i)
+
+
