5da4e4cd1febcbcc74415461a36e84ac23afa267
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-02-23 11:35:08 -0300

    ficha15 ejemplo multiplicador de arreglo

---
+++ ficha15/arreglo_multiplicado.py
@@ -0,0 +1,15 @@
+def arregloMulti(arregloUsuario, multiplicador):
+    arreglo = []
+    for i in arregloUsuario:
+        arreglo.append(i * multiplicador)
+
+    return arreglo
+
+
+arreglo_1 = [1,2,3,4,5,6,7,8,9,10]
+
+print(arregloMulti(arreglo_1, 2))
+
+print(arregloMulti(arreglo_1, 3))
+
+print(arregloMulti(arreglo_1, 200))
