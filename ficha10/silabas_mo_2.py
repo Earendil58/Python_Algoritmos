b5d3561a48904a6865ca291d48b8fff7b60af1a0
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-06-10 10:55:59 -0300

    practica parcial 2, unidad 10, ejercicio 1

--- ficha10/silabas_mo_2.py
+++ ficha10/silabas_mo_2.py
@@ -8,6 +8,9 @@ cant_palabras = 0
 palabra_mas_4_letras = 0
 aparecio_x_o_y = False
 palabras_al_menos_una_vez_x_o_y = 0
+anterior = ''
+aparecio_mo = False
+palabra_tiene_solo_una_vez_mo = 0
 
 for letra in texto_normalizado:
 
@@ -20,6 +23,7 @@ for letra in texto_normalizado:
 
         cant_letras = 0
         aparecio_x_o_y = False
+        aparecio_mo = False
 
 
     else:
@@ -30,6 +34,13 @@ for letra in texto_normalizado:
             aparecio_x_o_y = True
             palabras_al_menos_una_vez_x_o_y += 1
 
+        if letra == 'o' and anterior == 'm' and not(aparecio_mo):
+            aparecio_mo = True
+            palabra_tiene_solo_una_vez_mo += 1
+
+
+        anterior = letra
+
 
 promedio_letras_texto =round(cant_letras_totales / cant_palabras, 2)
 
@@ -37,6 +48,7 @@ promedio_letras_texto =round(cant_letras_totales / cant_palabras, 2)
 print(f'La cantidad de palabras con más de 4 letras son: {palabra_mas_4_letras}')
 print(f'La cant de palabras en las que aparecen al menos una vez "x" o "y" son: {palabras_al_menos_una_vez_x_o_y}')
 print(f'La cant de letras por palabra, en promedio, en el texto es de: {promedio_letras_texto}')
+print(f'La cant de veces que apareció una palabra, con sólo una vez la sílaba "mo" es: {palabra_tiene_solo_una_vez_mo}')
 
 
 
