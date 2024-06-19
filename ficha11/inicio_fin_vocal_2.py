3383c44009481d9d423090827499e7987dff56dc
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-06-19 16:23:12 -0300

    practica parcial 2, unidad 11, final ejercicio 1

--- ficha11/inicio_fin_vocal_2.py
+++ ficha11/inicio_fin_vocal_2.py
@@ -19,21 +19,19 @@ for letra in texto_normalizado:
 
         ultima_letra_palabra = anterior
 
-        print(f'la cant de letras es: {contador_letra}')
-
-        if primera_letra_palabra == ultima_letra_palabra:
-            cant_palabras_comienzan_como_termino_palabra_anterior += 1
-
         contador_letra = 0
-        primera_letra = True
 
     else:
         contador_letra += 1
 
-        if primera_letra and letra in vocales and contador_letra != 1:
-            comienzan_vocal += 1
-            primera_letra = False
-            print(f'Esta es la letra que se contó como vocal: {letra}')
+        if contador_letra == 1:
+            if letra in vocales:
+                comienzan_vocal += 1
+            if letra == ultima_letra_palabra:
+                cant_palabras_comienzan_como_termino_palabra_anterior += 1
+                print(f'Esta es la última letra: {ultima_letra_palabra}')
+
+        anterior = letra
 
 
 print(f'La cant de palabras en el texto que comienzan con vocales son: {comienzan_vocal}')
