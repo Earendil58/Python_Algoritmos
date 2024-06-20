6b7134b7296b3ae5f777311a51b3c6c5024b7347
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-06-20 12:14:29 -0300

    practica parcial 2, unidad 11, ejercicio: inicio y fin de vocal

--- ficha11/inicio_fin_vocal_2.py
+++ ficha11/inicio_fin_vocal_2.py
@@ -6,7 +6,9 @@ contador_palabras = 0
 contador_letra = 0
 anterior = ''
 vocales = 'aeiou'
-comienzan_vocal = 0
+comienza_vocal = 0
+termina_vocal = 0
+cant_palabras_comienzan_terminan_en_vocal = 0
 primera_letra = True
 primera_letra_palabra = ''
 ultima_letra_palabra = ''
@@ -15,26 +17,40 @@ cant_palabras_comienzan_como_termino_palabra_anterior = 0
 
 for letra in texto_normalizado:
 
+    contador_letra += 1
+
     if letra == ' ' or letra == '.':
 
-        ultima_letra_palabra = anterior
+        print(f'ULTIMA LETRA: {anterior}')
 
+        if contador_letra > 1:
+            ultima_letra_palabra = anterior
+
+        if ultima_letra_palabra in vocales:
+            termina_vocal = True
+
+        if comienza_vocal and termina_vocal:
+            cant_palabras_comienzan_terminan_en_vocal += 1
+
+        comienza_vocal = False
+        termina_vocal = False
         contador_letra = 0
 
     else:
-        contador_letra += 1
-
         if contador_letra == 1:
+            print(f'Primera letra: {letra}')
             if letra in vocales:
-                comienzan_vocal += 1
+                comienza_vocal = True
+
+
             if letra == ultima_letra_palabra:
                 cant_palabras_comienzan_como_termino_palabra_anterior += 1
-                print(f'Esta es la última letra: {ultima_letra_palabra}')
+
 
         anterior = letra
 
 
-print(f'La cant de palabras en el texto que comienzan con vocales son: {comienzan_vocal}')
+print(f'La cant de palabras en el texto que comienzan y terminan con vocales son: {cant_palabras_comienzan_terminan_en_vocal}')
 print(f'La cant de palabras en el texto que comienzan con la misma letra que termina la palabr anterior es de: {cant_palabras_comienzan_como_termino_palabra_anterior}')
 
 
