c1f426e7cc8f7c0d8f1a488c09c4f7d9d2334d03
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-06-25 17:11:41 -0300

    practica parcial 2, bla bla

--- random_test.py
+++ random_test.py
@@ -1,50 +1,119 @@
 __author__ = 'Catedra de Algoritmos y Estructuras de Datos'
 
-print('Analisis de texto con silabra dre')
-print('-' * 80)
 
-texto = input('Ingrese el texto a analizar, debe finalizar con punto: ')
-anterior = ''
-cont_letras = pal_3let = cant_pal = pal_terminan_s = pal_dre = 0
-tiene_d = tiene_dr = tiene_dre = False
-for letra in texto:
-    if letra == ' ' or letra == '.':
-        if cont_letras > 0:
-            cant_pal += 1
+def es_letra(car):
+    return 'a' <= car <= 'z' or 'A' <= car <= 'Z'
 
-            if cont_letras == 3:
-                pal_3let += 1
 
-            if anterior == 's':
-                pal_terminan_s += 1
+def es_letra_minuscula(car):
+    return 'a' <= car <= 'z'
 
-            if tiene_dre:
-                pal_dre += 1
 
-            tiene_dre = False
-            cont_letras = 0
-    else:
-        cont_letras += 1
-        if letra == 'd':
-            tiene_d = True
-            tiene_dr = False
+def es_vocal(car):
+    return car in 'aeiouáéíóúAEIOUÁÉÍÓÚ'
+
+
+def es_consonante(car):
+    return es_letra(car) and not es_vocal(car)
+
+
+def es_digito(letra):
+    return '0' <= letra <= '9'
+
+
+def es_impar(digito):
+    return int(digito) % 2 == 1
+
+
+def calcular_promedio(cantidad, total):
+    promedio = 0
+    if total > 0:
+        promedio = cantidad // total
+    return promedio
+
+
+def principal():
+    # el archivo "entrada.txt" está en la misma carpeta de este programa, en el repositorio GitLab. También está
+    # disponible en la Guía 14 junto con el enunciado, para ser descargado.
+    m = open("simulacro_parcial_2/simulacro_2024_01.txt")
+    texto = m.read()
+    print(texto)
+    m.close()
+
+    # para hacer pruebas cargando el texto por teclado, comentarice las tres líneas anteriores (en las que el
+    # texto se carga desde un archivo), y quite el comentario de la línea que sigue (en la que el texto se carga
+    # por teclado...
+    # texto = input("Ingresar texto (con punto al final): ")
+
+    cl = 0
+    r1 = r2 = r3 = r4 = 0
+    cant_voc = cant_cons = 0
+    clc = cpc = 0
+    cant_d = 0
+    anterior = ''
+    pal_mas_larga = None
+
+    inicia_digito = solo_letras_min = inicia_vocal = tiene_b = tiene_m = tiene_a = False
+
+    for car in texto:
+        if car != " " and car != ".":
+            cl += 1
+
+            if es_digito(car):
+                if es_impar(car) and cl == 1:
+                    inicia_digito = True
+
+            elif es_vocal(car):
+                cant_voc += 1
+                if cl == 1:
+                    inicia_vocal = True
+                if car == 'a' or car == 'A':
+                    tiene_a = True
+                if anterior == 'd' or anterior == 'D':
+                    cant_d += 1
+
+            elif es_consonante(car):
+                cant_cons += 1
+                if car == 'b' or car == 'B':
+                    tiene_b = True
+                elif car == 'm' or car == 'M':
+                    tiene_m = True
+
+            if cl == 2 and es_letra_minuscula(car):
+               solo_letras_min = True
+            elif not es_letra_minuscula(car):
+                solo_letras_min = False
+
+            anterior = car
+
         else:
-            if letra == 'r' and tiene_d:
-                tiene_dr = True
-            else:
-                if letra == 'e' and tiene_dr:
-                    tiene_dre = True
-                tiene_dr = False
-            tiene_d = False
-        anterior = letra
+            if inicia_digito and solo_letras_min:
+                r1 += 1
 
-porc = 0
-if cant_pal != 0:
-    porc = round(pal_3let * 100 / cant_pal, 2)
+            if inicia_vocal and tiene_b:
+                if pal_mas_larga is None:
+                    pal_mas_larga = cl
+                elif cl > pal_mas_larga:
+                    pal_mas_larga = cl
 
-print('Presentacion de Resultados')
-print('-' * 80)
-print('Cantidad de palabras con exactamente tres letras:', pal_3let)
-print('La cantidad de palabras que terminan con \'s\' son:', pal_terminan_s)
-print('La cantidad de palabras que contienen \'dre\' son:', pal_dre)
-print('El porcentaje de palabra de tres letras respecto del total de palabras del texto es: ', porc, '%', sep='')
+            if cant_cons > cant_voc and not tiene_m and not tiene_a:
+                clc += cl
+                cpc += 1
+
+            if cant_d >= 2 and es_vocal(anterior):
+                r4 += 1
+
+            cl = cant_voc = cant_cons = cant_d = 0
+            inicia_digito = solo_letras_min = inicia_vocal = tiene_b = tiene_a = tiene_m = False
+
+    r2 = pal_mas_larga
+    r3 = calcular_promedio(clc, cpc)
+
+    print('Primer resultado:', r1)
+    print('Segundo resultado:', r2)
+    print('Tercer resultado:', r3)
+    print('Cuarto resultado:', r4)
+
+
+if __name__ == '__main__':
+    principal()
--- simulacro_parcial_2/simularo_2024_01.py
+++ simulacro_parcial_2/simularo_2024_01.py
@@ -4,10 +4,24 @@ def es_letra(caracter):
 def es_letra_minuscula(caracter):
     return 'a' <= caracter <= 'z'
 
+def es_vocal(caracter):
+    vocales = 'aeiouáéíóúAEIOUÁÉÍÓÚ'
+    return caracter in vocales
 
-def es_numero(letra):
-    return '0' <= letra <= '9'
+def es_consonante(caracter):
+    return es_letra(caracter) and not es_vocal(caracter)
 
+def es_numero(caracter):
+    return '0' <= caracter <= '9'
+
+def es_impar(caracter):
+    return int(caracter) % 2 != 0
+
+def calcular_promedio(cantidad, total):
+    promedio = 0
+    if total > 0:
+        promedio = cantidad // total
+    return promedio
 
 
 
@@ -23,38 +37,78 @@ def principal():
     cant_caracter_por_palabra = 0
     primer_caracter_digito = False
     letras_minusculas_dsp_de_caracter_numerico = True
+    longitud_palabra_mas_larga = 0
+    comienza_vocal = False
+    contiene_b = False
+    cant_letras_consonantes = 0
+    cant_palabras_consonantes = 0
+    cant_vocales = 0
+    cant_consonantes = 0
+    tiene_m = tiene_a = False
 
 
     for letra in texto:
         if letra == ' ' or letra == '.':
 
-            if letras_minusculas_dsp_de_caracter_numerico:
+            if primer_caracter_digito and letras_minusculas_dsp_de_caracter_numerico:
                 cant_palabras_comienzan_digito_siguen_letras_minusculas += 1
 
+            elif comienza_vocal and contiene_b:
+                if longitud_palabra_mas_larga == 0:
+                    longitud_palabra_mas_larga = cant_caracter_por_palabra
+                else:
+                    if cant_caracter_por_palabra > longitud_palabra_mas_larga:
+                        longitud_palabra_mas_larga = cant_caracter_por_palabra
+
+            if cant_consonantes > cant_vocales:
+                cant_letras_consonantes += cant_caracter_por_palabra
+                cant_palabras_consonantes += 1
+
+
             cant_caracter_por_palabra = 0
-            primer_caracter_digito = False
-            letras_minusculas_dsp_de_caracter_numerico = True
+            primer_caracter_digito = False # R1 FLAGS
+            letras_minusculas_dsp_de_caracter_numerico = True  # R1 FLAGS
+            comienza_vocal = contiene_b = False  # R2 FLAGS
+            cant_consonantes = cant_vocales = 0  # R3 FLAGS
+
 
 
         else:
             cant_caracter_por_palabra += 1
 
-            if cant_caracter_por_palabra == 1 and es_numero(letra):
-                primer_caracter_digito = True
+            if cant_caracter_por_palabra == 1:
+                if es_numero(letra) and es_impar(letra):
+                    primer_caracter_digito = True
 
-            if primer_caracter_digito and not(es_letra_minuscula(letra)):
-                letras_minusculas_dsp_de_caracter_numerico = False
+                elif es_vocal(letra):
+                    comienza_vocal = True
+
+            elif cant_caracter_por_palabra > 1:
+                if not(es_letra_minuscula(letra)):
+                    letras_minusculas_dsp_de_caracter_numerico = False
+
+                elif letra == 'b':
+                    contiene_b = True
+
+            if es_vocal(letra) and (letra != 'a' and letra != 'A'):
+                cant_vocales += 1
+
+            if es_consonante(letra) and (letra != 'm' and letra != 'M'):
+                cant_consonantes += 1
 
 
 
-        r1 = cant_palabras_comienzan_digito_siguen_letras_minusculas
-            
+
+    r1 = cant_palabras_comienzan_digito_siguen_letras_minusculas
+    r2 = longitud_palabra_mas_larga
+    r3 = calcular_promedio(cant_letras_consonantes, cant_palabras_consonantes)
+
 
 
 
     print("Primer resultado:", r1)
-    # print("Segundo resultado:", r2)
-    # print("Tercer resultado:", r3)
+    print("Segundo resultado:", r2)
+    print("Tercer resultado:", r3)
     # print("Cuarto resultado:", r4)
 
 
@@ -62,4 +116,5 @@ def principal():
 
 if __name__ == '__main__':
     principal()
+
 
