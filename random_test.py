00a9adda370380f82532a2e583fc4b409636fe2e
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-07-15 12:40:46 -0300

    unidad 15, estudio

--- random_test.py
+++ random_test.py
@@ -1,30 +1,24 @@
-vector = ['a', 'b', 'c', 'd', 'e']
+notas = []
 
-print(f'Este es el vector: {vector}')
+for nota in range(4):
+    nota = int(input('Ingrese una nota: '))
+    notas.append(nota)
 
-print(f'Este es el primer indice del vector: {vector[0]}')
 
-cadena = 'Hola, soy una cadena'
+print(f'Las notas fueron: {notas}')
 
-print(f'Este es el primer elemento de la cadena: {cadena[0]}')
 
-print(f'Estos son varios elementos de la cadena: {cadena[0:4]}')
+suma_notas = 0
 
-vector[0] = 'A'
+print(len(notas))
 
-print(vector)
+for nota in range(len(notas)):
+    suma_notas += notas[nota]
 
-vector_2 = 10 * ['Hola']
 
-print(vector_2)
+promedio = suma_notas / 4
 
-vector_3 = list(cadena)
-
-print(vector_3)
-
-vector_4 = [i for i in range(1, 10 + 1)]
-
-print(vector_4)
+print(f'El promedio es: {promedio}')
 
 
 
