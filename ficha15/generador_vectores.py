8d6cba9b2b272cf3be65cf7549af02ee7c0cf3d6
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-07-22 12:31:24 -0300

    unidad 15, suma arrays

---
+++ ficha15/generador_vectores.py
@@ -0,0 +1,34 @@
+def generador_arrays(cantArrays):
+    conjunto_arrays = []
+
+    if cantArrays > 0:
+        for i in range(cantArrays):
+            array = []
+            conjunto_arrays.append(array)
+        return conjunto_arrays
+
+def popular_arrays(conjuntoArrays):
+    for i in range(len(conjuntoArrays)):
+        if len(conjuntoArrays[i]) == 0:
+            tamaño_array = int(input(f'Ingrese el tamaño del array {i + 1}: '))
+            array = []
+            for elemento in range(tamaño_array):
+                numero = int(input('Ingrese un valor para el elemento del array: '))
+                array.append(numero)
+            conjuntoArrays[i] = array
+    print(f'Los arrays populados: {conjuntoArrays}')
+    return conjuntoArrays
+
+
+def test():
+    cant_arrays = int(input('Ingrese la cantidad de arrays a generar: '))
+    arrays_a_popular = generador_arrays(cant_arrays)
+    popular_arrays(arrays_a_popular)
+
+if __name__ == '__main__':
+    test()
+
+
+
+
+
