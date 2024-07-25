aa9312c33f4ad395a600d58225df6348296db946
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-07-25 12:19:47 -0300

    unidad 15, pluviometro 2

--- ficha15/pluviometro.py
+++ ficha15/pluviometro.py
@@ -9,6 +9,26 @@ def promedio_anual(lluvias):
 
     return promedio_anual_lluvias
 
+def promedio_lluvias_trimestre(lluvias, trimestre):
+    trimestre_empieza = None
+    trimestre_termina = None
+    if trimestre == 1:
+        trimestre_empieza = 0
+        trimestre_termina = 3
+    elif trimestre == 2:
+        trimestre_empieza = 3
+        trimestre_termina = 6
+    elif trimestre == 3:
+        trimestre_empieza = 6
+        trimestre_termina = 9
+    elif trimestre == 4:
+        trimestre_empieza = 9
+        trimestre_termina = 12
+
+    lluvia_trimestre_elegido = lluvias[trimestre_empieza:trimestre_termina]
+
+    print(lluvia_trimestre_elegido)
+
 
 
 def test():
@@ -22,6 +42,13 @@ def test():
     promedio_anual_lluvias = promedio_anual(lluvias_registradas)
 
     print(promedio_anual_lluvias)
+
+    trimestre_elegido = int(input('Ingrese el trimestre elegido para validar el promedio (1,2,3,4): '))
+
+    lluvias_por_trimestre = promedio_lluvias_trimestre(lluvias_registradas, trimestre_elegido)
+
+    print(lluvias_por_trimestre)
+
 
 
 if __name__ == '__main__':
