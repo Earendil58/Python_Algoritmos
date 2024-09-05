42ce00ae3430cf7754fc6324c523e57772439039
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-09-05 11:47:36 -0300

    Unidad 19, puntos cartesianos

---
+++ ficha19/puntos_cartesianos.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+class Punto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def dist_origen(self):
+        return sqrt(self.x ** 2 + self.y ** 2)
+
+
+
+punto_1 = Punto(1.5,7.5)
+
+print(f'Valor de x: {punto_1.x}')
+print(f'Valor de y: {punto_1.y}')
+
+print(f'La distancia al origen, desde el punto, es: {punto_1.dist_origen()}')
