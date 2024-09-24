1c4d4c80b77e3ac152bcf145209d64de23d3cabb
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2024-09-24 12:21:22 -0300

    unidad 21, recorrido de una matriz

--- random_test.py
+++ random_test.py
@@ -1,9 +1,32 @@
+
+# arreglo = []
+#
+# for i in range(3):
+#     arreglo.append([])
+#     for j in range(3):
+#         arreglo[i].append(None)
+#
+# print(arreglo)
+#
+#
+# arreglo_1 = [0] * 3
+# for j in range(3):
+#     arreglo_1[j] = [0] * 3
+#
+# print(arreglo_1)
+#
+#
+# arreglo_2 = [[1] * 3 for f in range(3)]
+#
+# print(arreglo_2)
+
 matriz = [['a', 'b', 'c'],
           ['d', 'e', 'f'],
           ['g', 'h', 'i']]
 
-print(matriz)
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        print(matriz[i][j])
 
-print(f'Primer elemento, de la primera fila: {matriz[0][0]}')
 
-print(f'Elemento X23: {matriz[1][2]}')
+
