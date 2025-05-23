c1715e291affaa1cf53274de41c15ff881b5fe0a
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2025-05-23 11:06:21 -0300

    Generador de mails

--- random_test.py
+++ random_test.py
@@ -1,19 +1,9 @@
 
+nombre = input('Ingrese su nombre: ')
+apellido = input('Ingrese su apellido: ')
+dominio = input('Ingrese su dominio: ')
 
-palabra_a_enmascarar = input('Ingrese la palabra: ')
-palabra_enmascarada = ''
-longitud_palabra = len(palabra_a_enmascarar)
-contador = 0
-
-for letra in palabra_a_enmascarar:
-    contador += 1
-    if contador == 1 or contador == longitud_palabra:
-        palabra_enmascarada += letra
-    else:
-        palabra_enmascarada += '*'
-
-
-
-
-
-print(f'La palabra enmascarada es: {palabra_enmascarada}')
+if nombre[0] == apellido[0]:
+    print(f'La direccion de mail es : {nombre}.{apellido}@{dominio}')
+else:
+    print(f'La direccion de mail es : {nombre[0]}.{apellido}@{dominio}')
