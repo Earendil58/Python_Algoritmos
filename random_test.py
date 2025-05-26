cf6f46d56bf303bf8d235044ead5f19c221db24d
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2025-05-26 12:33:20 -0300

    doble de impar, final

--- random_test.py
+++ random_test.py
@@ -1,30 +1,19 @@
 #Diseña un programa que, dado un número entero, determine si este es el doble de un número impar. (Ejemplo: 14 es el doble de 7, que es impar).
 
 numero = float(input("Digite um numero: "))
-divisores = numero - 1
-divisor = None
-impar_doble_de_par = False
 
-#NUMERO POSITIVO E IMPAR:
-if numero > 0 and numero % 2 != 0:
-    while numero % divisores != 0:
-        divisores -= 1
+if not numero.is_integer():
+    print("El número ingresado no es entero.")
 
-    divisor = divisores
+if numero % 2 == 0:
+    if (numero // 2) % 2 != 0:
+        print(f"El número {numero} es el doble de impar")
 
-    if divisores == 1:
-        impar_doble_de_par = True
-    else:
-        print(f'El número {numero} es impar. No es doble de impares')
+    elif (numero // 2) % 2 == 0:
+        print(f'El número {numero} es par pero no es doble de impar')
 
-
-
- #NUMERO PAR
 else:
-    print(f'El número: {numero} es par, por lo que no es el doble de un número impar')
+    print(f'El número {numero} es impar')
 
-
-if impar_doble_de_par:
-    print(f'El número: {numero} es impar, doble de par.')
 
 
