dfe208297ce02037cd4d6305c3c9d29dc755f065
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2025-06-02 16:50:32 -0300

    caracter mayuscula o minuscula

--- random_test.py
+++ random_test.py
@@ -1,24 +1,10 @@
-primer_numero = int(input('Ingrese el primer numero: '))
-segundo_numero = int(input('Ingrese el segundo numero: '))
-tercero_numero = int(input('Ingrese el tercero numero: '))
+letra = input("Digite uma letra: ")
 
-mayor = None
+if "a" < letra < "z":
+    print('El caracter es una letra minúscula')
 
-if primer_numero > segundo_numero:
-    if primer_numero > tercero_numero:
-        mayor = primer_numero
-    else:
-        mayor = tercero_numero
-
-elif segundo_numero > primer_numero:
-    if segundo_numero > tercero_numero:
-        mayor = segundo_numero
-    else:
-        mayor = tercero_numero
+elif "A" < letra < "Z":
+    print(f'El caracter ingresado {letra} es una letra mayúscula')
 
 else:
-    if segundo_numero < tercero_numero or primer_numero < tercero_numero:
-        mayor = tercero_numero
-
-
-print(f'El mayor de los numeros ingresados: {mayor}')
+    print(f'El caracter ingresado {letra} no es una letra')
